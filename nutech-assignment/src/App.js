771cfdf93f1a6d33b3198c6e556d0290c1bca50e
KevDeemz <kevindimas@gmail.com>
2025-07-31 23:07:52 +0700

    next

--- nutech-assignment/src/App.js
+++ nutech-assignment/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Homepage from './Pages/Homepage';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Transaction from './Pages/Transaction';
 import Login from './Pages/Login';
 import  Register  from './Pages/Register';
@@ -8,18 +8,15 @@ import Userprofile from './Pages/Userprofile';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/transaction" element={<Transaction />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path='/profile' element={<Userprofile />} />
-        </Routes>
-      </Router>
-    </BrowserRouter>
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path='/profile' element={<Userprofile />} />
+      </Routes>
+    </Router>
   );
 }
 
